812b5e14ea7e2ed77ab5fdc3eaf49efab4eaf7ef
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-06-30 08:20:54 -0300

    add route for client index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :states
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "/client", to: "clients#index"
   # Defines the root path route ("/")
   root "states#index"
 end
