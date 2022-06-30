28203f1c64208e74dbdba5a0706370e4e1f7d19d
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-06-30 08:13:13 -0300

    create migrate and schema for model clients

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_26_150038) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_29_003930) do
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "states", force: :cascade do |t|
     t.string "name"
     t.string "attachment"
