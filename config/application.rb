6bebd999cdea5b3224ab90cd18c9217d7d57982a
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-07-07 21:32:54 -0300

    add config for executor queue sidekiq

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module ReadFileV3
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.active_job.queue_adapter = :sidekiq       #configura quem ira executar a fila queue
   end
 end
