df5b32fbcad6eb77c8cdba72be501541c07c2f8d
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-07-07 21:39:49 -0300

    fixed route sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   resources :states
 
-  mout Sidekiq::web => '/sidekiq'
+  mount Sidekiq::Web => '/sidekiq' #caminho para sidekiq/web  
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/client", to: "clients#index"
