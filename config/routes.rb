8e760faecdbf646a572c9244f6d2af06ba7610fe
Lucas-SilvaGit <lucasnba2324@gmail.com>
2022-07-07 21:34:45 -0300

    add route for sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   resources :states
+
+  mout Sidekiq::web => '/sidekiq'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/client", to: "clients#index"
   # Defines the root path route ("/")
